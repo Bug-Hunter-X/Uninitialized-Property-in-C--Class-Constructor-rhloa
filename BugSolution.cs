9848df6c16399bba346bde113ae7fd551fd635da
BugHunter-X <askzenai@gmail.com>
2024-12-22 22:26:04 +0100

    Add BugSolution.cs file

---
+++ BugSolution.cs
@@ -0,0 +1,8 @@
+public class MyClass {
+    public int MyProperty { get; set; }
+
+    public MyClass() {
+        // Explicitly initialize MyProperty to the desired default value
+        MyProperty = 10; // Or any other suitable value
+    }
+}
